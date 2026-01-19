be328dd43f9c6c725ecdb06c6fc13fadcb14a712
Bala Subramanyam Duggirala <subramanyam6666@Mac.lan>
2026-01-19 11:17:08 -0600

    Add default PORT fallback in nginx startup script

--- portfolio-app/frontend/start-nginx.sh
+++ portfolio-app/frontend/start-nginx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Substitute PORT environment variable in nginx config
+# Substitute PORT environment variable in nginx config (default to 8080 if not set)
+export PORT=${PORT:-8080}
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
