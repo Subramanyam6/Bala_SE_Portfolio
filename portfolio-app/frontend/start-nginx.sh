4636ba6c682098ea6ee1a8485c15814fbff61cef
Bala Subramanyam Duggirala <subramanyam6666@Mac.lan>
2026-01-19 11:16:59 -0600

    Fix nginx port configuration for Render.com PORT environment variable

---
+++ portfolio-app/frontend/start-nginx.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Substitute PORT environment variable in nginx config
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+exec nginx -g 'daemon off;'
